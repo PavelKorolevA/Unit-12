32c70d5bcdf034eb03a3f903078c4d41b57a798a
p.korolev <amlerok96@gmail.com>
2022-04-14 23:08:05 +0300

    fix

--- src/test/java/korolev/qa/FilesParsingTest.java
+++ src/test/java/korolev/qa/FilesParsingTest.java
@@ -1,66 +1,64 @@
 package korolev.qa;
 
 import com.codeborne.pdftest.PDF;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
-import static com.codeborne.selenide.Selectors.byText;
 
 public class FilesParsingTest {
 
-    ClassLoader classLoader = getClass().getClassLoader();
-
     @Test
-    void parsePdfTest() throws Exception {
-        Selenide.open("https://junit.org/junit5/docs/current/user-guide/ ");
-        File pdfDownload = Selenide.$(byText("PDF download")).download();
-        PDF pdf = new PDF(pdfDownload);
-        assertThat(pdf.author).contains("Marc Philipp");
-        assertThat(pdf.numberOfPages).isEqualTo(166);
-    }
+    void parseZipTest() throws Exception {
+        File file = new File("src/test/resources/files/Unit12");
+        ZipFile zipFile = new ZipFile(file.getAbsolutePath());
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            switch (entry.getName()) {
+                case "Machine_readable_file_bdc_sf_2021_q4.csv":
+                    CSVReader reader = new CSVReader(new InputStreamReader(zipFile.getInputStream(entry)));
+                {
+                    List<String[]> content = reader.readAll();
+                    assertThat(content.get(0)).contains(
+                            "Series_reference",
+                            "Period",
+                            "Data_value",
+                            "Suppressed",
+                            "STATUS",
+                            "UNITS",
+                            "Magnitude",
+                            "Subject",
+                            "Group");
+                }
+                break;
 
-
-    @Test
-    void parseXlsTest() throws Exception {
-        Selenide.open("http://romashka2008.ru/price");
-        File xlsDownload = Selenide.$(".site-main__inner a[href*='prajs_ot']").download();
-        XLS xls = new XLS(xlsDownload);
-        assertThat(xls.excel
-                .getSheetAt(0)
-                .getRow(11)
-                .getCell(1)
-                .getStringCellValue()).contains("693010, Сахалинская обл, Южно-Сахалинск г");
-    }
-
-    @Test
-    void parseCsvTest() throws Exception {
-        try (InputStream is = classLoader.getResourceAsStream("files/business-financial-data-sep-2021-quarter.csv");
-             CSVReader reader = new CSVReader(new InputStreamReader(is))) {
-            List<String[]> content = reader.readAll();
-            Assertions.assertThat(content.get(0)).contains(
-                    "Series_reference",
-                    "Period",
-                    "Data_value",
-                    "Suppressed",
-                    "STATUS",
-                    "UNITS",
-                    "Magnitude",
-                    "Subject",
-                    "Group",
-                    "Series_title_1",
-                    "Series_title_2",
-                    "Series_title_3",
-                    "Series_title_4",
-                    "Series_title_5");
+                case "prajs_ot_1204.xls": {
+                    InputStream is = zipFile.getInputStream(entry);
+                    XLS xls = new XLS(is);
+                    assertThat(xls.excel
+                            .getSheetAt(0)
+                            .getRow(11)
+                            .getCell(1)
+                            .getStringCellValue()).contains("693010, Сахалинская обл, Южно-Сахалинск г");
+                    break;
+                }
+                case "52c8e8114c11cfa9e6e668ee652dbf2b.pdf": {
+                    InputStream is = zipFile.getInputStream(entry);
+                    PDF pdf = new PDF(is);
+                    assertThat(pdf.text).contains("Пример PDF файла");
+                    break;
+                }
+            }
         }
     }
 }
